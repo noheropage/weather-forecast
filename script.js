4581f3c695375431f9b2ddc9e7e5b81039b93ae8
noheropage <mursepage@gmail.com>
2021-02-02 18:11:36 -0800

    Planning out major code components

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// TODO: search bar that accepts name of city
+
+// TODO: Put that city name into weather API fetch
+
+// TODO: store city names in local storage and display on side
+var h = localStorage.getItem("history") || []
+
+// TODO: Display current info 
+
+// TODO: Display 5-Day Forecast with icons
