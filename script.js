9b1efe3c04177387b2992ca3d3c9b2d070127191
noheropage <mursepage@gmail.com>
2021-02-03 13:34:43 -0800

    Format cleanup and comments

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var month;
 var year;
 var day;
 var formattedDate;
-var cityBtn = $("<button>")
 
+// look through local storage to populate list of buttons of cities previously searched OR create new array
 var h = JSON.parse(localStorage.getItem("oldCity")) || []
 function parseLocal() {
     $(".search-history").empty()
@@ -16,28 +16,39 @@ function parseLocal() {
         $(cityBtn).attr("id", "searchedCities")
         $(cityBtn).text(h[i])
         $(cityBtn).val(h[i])
+        // add buttons so that most recent are at the top
         $(".search-history").prepend(cityBtn)
     }
+    localStorage.setItem("oldCity", JSON.stringify(h))
+
 }
 
-localStorage.setItem("oldCity", JSON.stringify(h))
+// place everything in 'h' into local storage
+// localStorage.setItem("oldCity", JSON.stringify(h))
 
+// click event for all buttons new and old
 $(document).on("click", ".btn", function (event) {
     event.preventDefault();
-    // console.log(this);
-    console.log($(this));
+    // will trigger api search based on value of field targetted
     var city = $("#searchCity").val() || $(this).val();
+    // reset input bar once search button is clicked
     $("#searchCity").val('');
+    
+    // check to make sure there's something to do an api call for
     if (!city) {
         console.error("You must enter the name of a city");
         return;
     }
+
+    // clear out info fields prior to populating them 
     $("#currentWeather").empty()
     $("#weatherForecast").empty()
+    // store most recent search in localstorage
     localStorage.setItem("oldCity", JSON.stringify(h))
     searchApi(city)
 })
 
+// uses latitude and longitude to find the uvi
 function uvIndex(lat, lon) {
     var apiCallUv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
     console.log(apiCallUv);
@@ -45,7 +56,6 @@ function uvIndex(lat, lon) {
         if (!response.ok) {
             throw response.json();
         }
-        // console.log(response.json());
         return response.json();
     })
         .then(function (uv) {
@@ -56,7 +66,7 @@ function uvIndex(lat, lon) {
         });
 }
 
-
+// prints uvi and color codes it
 function printUv(uv) {
     var uvValue = uv.current.uvi
     var uvText = $("<p>")
@@ -74,6 +84,8 @@ function printUv(uv) {
 function printCurrent(result) {
     var currentWeather = $("#currentWeather")
     $(currentWeather).attr("class", "border rounded-sm p-3")
+
+    // variables related to date
     unixTimestamp = result.dt
     date = new Date(unixTimestamp * 1000);
     month = date.getMonth();
@@ -81,26 +93,31 @@ function printCurrent(result) {
     day = date.getDate();
     formattedDate = month + "/" + day + "/" + year
 
+    // sets up a img tag to link an icon image from website based on icon code from api
     var iconImg = $("<img>")
     var iconCode = result.weather[0].icon
     var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
     $(iconImg).attr('src', iconUrl)
     $(currentWeather).append(iconImg)
+
+    // check if city has already been searched for and only adds it to history if it's new
     if (jQuery.inArray(result.name, h) == -1) {
         h.push(result.name)
         parseLocal();
     }
 
+    // use latitude and longitude from api call to use in uv api call
     var lat = result.coord.lat
     var lon = result.coord.lon
     uvIndex(lat, lon)
 
+    // output for current weather conditions
     $(currentWeather).append("<h2>" + result.name + " (" + formattedDate + ") </h2>")
     $(currentWeather).append("<p>Temperature: " + result.main.temp + " &degF </p>")
     $(currentWeather).append("<p>Humidity: " + result.main.humidity + "% </p>")
     $(currentWeather).append("<p>Wind Speed: " + result.wind.speed + "MPH</p>")
-    // $(currentWeather).append("<p>UV Index: " +  + "</p>")  // TODO: UV index 
 }
+
 
 function printForecast(result) {
     var weatherForecast = $("#weatherForecast")
@@ -108,6 +125,7 @@ function printForecast(result) {
     $(forecastTitle).attr("class", "container")
     $(weatherForecast).append(forecastTitle)
     $(forecastTitle).append("<h3>5-Day Forecast: </h3>")
+    // api call produces 40 results; increasing the index by 8 gives us 5 days of forecasting data
     for (var i = 7; i < result.list.length; i += 8) {
         unixTimestamp = result.list[i].dt
         date = new Date(unixTimestamp * 1000);
