b116d7a90993d79e7c472c0b8b7f6fa6ff55dc15
noheropage <mursepage@gmail.com>
2021-02-03 02:39:14 -0800

    Convert UTC from openweather api to readable format

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 var apiKey = "9ffba577a38eec73c530d29d74f789eb"
+// var date = new Date(); 
 
-
-// TODO: search bar that accepts name of city
-
-
-// console.log(apiCall);
-    // TODO: search button 
-$("#searchBtn").on("click", function(event) {
+$("#searchBtn").on("click", function (event) {
     event.preventDefault();
     var city = $("#searchCity").val();
     if (!city) {
@@ -17,37 +12,82 @@ $("#searchBtn").on("click", function(event) {
     searchApi(city)
 })
 
-function printResult(result) {
+function printCurrent(result) {
 
     var currentWeather = $("#currentWeather")
-    var weatherForecast = $("#weatherForecast")
-    $(currentWeather).append("<p>" + result.name + "</p>")
+    var unixTimestamp = result.dt
+    var date = new Date(unixTimestamp * 1000);
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    var day = date.getDay();
+    var formattedDate = month + "/" + day + "/" + year
+
+    // TODO: style output
+    $(currentWeather).append("<h2>" + result.name + " " + formattedDate + "</h2>")
     $(currentWeather).append("<p>Temperature: " + result.main.temp + " &degF </p>")
     $(currentWeather).append("<p>Humidity: " + result.main.humidity + "% </p>")
     $(currentWeather).append("<p>Wind Speed: " + result.wind.speed + "MPH</p>")
     $(currentWeather).append("<p>UV Index: </p>")  // TODO: UV index 
-    
+
+
+
     console.log(result.name);
+}
+
+function printForecast(result) {
+    var weatherForecast = $("#weatherForecast")
+    for (var i = 0; i < result.list.length; i += 8) {
+        var unixTimestamp = result.list[i].dt
+        var date = new Date(unixTimestamp * 1000);
+        var month = date.getMonth();
+        var year = date.getFullYear();
+        var day = date.getDay();
+        var formattedDate = month + "/" + day + "/" + year
+        $(weatherForecast).append("<p>" + formattedDate + "</p>")
+        var iconImg = $("<img>")
+        var iconCode = result.list[i].weather[i].icon
+        console.log(iconCode);
+        var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+        $(iconImg).attr('src', iconUrl)
+        $(weatherForecast).append(iconImg)
+        $(weatherForecast).append("<p> Temp: " + result.list[i].main.temp + "&degF </p>")
+        $(weatherForecast).append("<p> Humidity: " + result.list[i].main.temp + "% </p>")
+    }
+
 }
 
 // TODO: Put that city name into weather API fetch
 function searchApi(city) {
-    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-    fetch(apiCall).then(function (response) {
+    var apiCallCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    var apiCallForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+    fetch(apiCallCurrent).then(function (response) {
         if (!response.ok) {
             throw response.json();
         }
-        console.log(apiCall);
-        // console.log(response.json())
+        console.log(apiCallCurrent);
         return response.json();
     })
-    .then(function (locRes) {
-        console.log(locRes);
-        printResult(locRes);
+        .then(function (current) {
+            console.log(current);
+            printCurrent(current);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+    fetch(apiCallForecast).then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        console.log(apiCallForecast);
+        return response.json();
     })
-    .catch(function (error) {
-        console.error(error);
-    });
+        .then(function (forecast) {
+            console.log(forecast);
+            printForecast(forecast);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 }
 
 
