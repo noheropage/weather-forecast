92fba85f8bb4b27cf3127285baaf046c74ebc1c2
noheropage <mursepage@gmail.com>
2021-02-02 18:31:34 -0800

    Add search bar and button on html

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 
 // TODO: search bar that accepts name of city
 
+    // TODO: search button 
+
 // TODO: Put that city name into weather API fetch
 
 // TODO: store city names in local storage and display on side
 var h = localStorage.getItem("history") || []
+localStorage.setItem("history", JSON.stringify(h))
+
+h.push("Portland")
+
+localStorage.setItem("history", JSON.stringify(h))
 
 // TODO: Display current info 
 
